c9099960a0ede4daf9106e1661dd5c75522a02d1
Jake Fordyce <jfordyce@covermymeds.com>
2020-09-01 14:28:00 -0400

    add seed data and add relationship between users and lists

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(email: 'jfordyce@covermymeds.com', password: 'testing123')
+
+List.create(name: 'Test List 1', user_id: 1)
