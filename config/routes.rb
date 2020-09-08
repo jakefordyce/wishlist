8a2e739361d6887a64e25ae7acf4a647aa354ea5
Jake Fordyce <jfordyce@covermymeds.com>
2020-09-08 09:30:17 -0400

    Add devise controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   resources :items
   resources :lists
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'users/registrations' }
+  devise_scope :user do
+    get 'users/:id', to: 'users/registrations#show'
+  end
   get 'static_pages/home'
   root to: "static_pages#home"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
