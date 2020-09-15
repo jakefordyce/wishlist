fa9e33700161256ce69c656584b06a01d3a51efe
Jake Fordyce <jfordyce@covermymeds.com>
2020-09-15 09:30:23 -0400

    add comments to list

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,8 @@
 
 User.create(email: 'jfordyce@covermymeds.com', username: 'jfordyce', password: 'testing123')
 
+User.create(email: 'jakefordyce78@gmail.com', username: 'jakefordyce78', password: 'testing123')
+
 List.create(name: 'Test List 1', user_id: 1)
 
 List.create(name: 'Test List 2', user_id: 1)
@@ -17,3 +19,7 @@ Item.create(name: 'A cool pen', list_id: 1, url: 'https://www.gouletpens.com/col
 Item.create(name: 'Same cool pen', list_id: 2, url: 'https://www.gouletpens.com/collections/flex-soft-nib-pens/products/noodlers-triple-tail-flex-fountain-pen-clear?variant=30284462850091')
 
 Item.create(name: 'A cool ink', list_id: 1, url: 'https://www.gouletpens.com/collections/bottled-ink/products/noodlers-54th-massachusetts-3oz-bottled-ink?variant=11884740509739')
+
+Comment.create(user_id: 1, list_id: 1, body: "wow nice fake list!")
+
+Comment.create(user_id: 2, list_id: 1, body: "Meh, I've seen better.")
