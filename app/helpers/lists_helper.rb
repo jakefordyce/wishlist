21eebc3454db0fcfba6053cb6e1d6d57cf5214a9
Jake Fordyce <jfordyce@covermymeds.com>
2020-09-16 13:01:35 -0400

    add header and hide comments from list creator

--- app/helpers/lists_helper.rb
+++ app/helpers/lists_helper.rb
@@ -1,2 +1,7 @@
 module ListsHelper
+
+  def list_belongs_to_user?(list)
+    list.user_id == current_user
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: { registrations: 'users/registrations' }
+  devise_scope :user do
+    get 'users/:id', to: 'users/registrations#show', as: 'profile'
+  end
   resources :comments
   resources :items
   resources :lists
-  devise_for :users, controllers: { registrations: 'users/registrations' }
-  devise_scope :user do
-    get 'users/:id', to: 'users/registrations#show'
-  end
+
   get 'static_pages/home'
   root to: "static_pages#home"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
