d1776a21e441f572f7ac0ca66fc4d6941a5d7cd6
Jake Fordyce <jfordyce@covermymeds.com>
2020-11-15 14:12:44 -0500

    prevent duplicate requests and collabs

--- app/models/collaboration.rb
+++ app/models/collaboration.rb
@@ -1,4 +1,5 @@
 class Collaboration < ApplicationRecord
   belongs_to :collaborator, class_name: "User"
   belongs_to :collab, class_name: "List"
+  validates_uniqueness_of :collaborator_id, scope: :collab_id
 end
