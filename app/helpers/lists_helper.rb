ceb3f2fae2c8cf3fa13b44320fd85e231fa2b1fe
Jake Fordyce <jfordyce@covermymeds.com>
2020-11-15 13:27:55 -0500

    hide request button when a request is pending

--- app/helpers/lists_helper.rb
+++ app/helpers/lists_helper.rb
@@ -12,7 +12,8 @@ module ListsHelper
   def show_request_button?(list)
     user_signed_in? &&
     !list_belongs_to_user?(list) &&
-    !Collaboration.exists?(collab: list, collaborator: current_user.id)
+    !Collaboration.exists?(collab: list, collaborator: current_user.id) &&
+    !CollabRequest.exists?(user: current_user, list: list)
   end
 
 end
