75304c9578e85f98d25c3ab4d1228febbfd9d1c9
Jake Fordyce <jfordyce@covermymeds.com>
2020-11-19 10:20:11 -0500

    display list of pending requests in header

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def pending_requests
+    result = "Requests "
+    if user_signed_in?
+      req_count = current_user.collab_requests.count
+      req_count += CollabRequest.joins(:list).where(lists: {user_id: current_user.id}, from_owner: false).count
+      result += req_count.to_s if req_count > 0
+    end
+    result
+  end
 end
