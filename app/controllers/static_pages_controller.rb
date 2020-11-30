6300aa65e950003a6534a20c99c2dd63b5e6c171
Jake Fordyce <jfordyce@covermymeds.com>
2020-11-30 14:00:10 -0500

    add Search page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -14,6 +14,7 @@ class StaticPagesController < ApplicationController
   def search
     filtered_users = User.joins(:collaborations).where(collaborations: {collab_id: params[:list_id]})
     filtered_users += User.where(id: current_user.id)
+    @list = List.find(params[:list_id])
 
     @users = User.ransack(username_cont: params[:q])
             .result(distinct: true)
